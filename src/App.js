274967e5227f87ab11ff530f0ad92238767ad60c
hsull <>
2021-05-05 22:47:08 +1000

    Add animation and social buttons

--- src/App.js
+++ src/App.js
@@ -4,28 +4,65 @@ import HeroText from './components/HeroText/HeroText';
 import ProfilePhoto from './resources/ProfilePhoto.jpg';
 import TitleText from './components/TitleText/TitleText';
 import { SocialIcon } from 'react-social-icons';
+import { ParallaxProvider } from 'react-scroll-parallax';
+import { Parallax } from 'react-scroll-parallax';
+import useWindowDimensions from './useWindowDimensions';
+import Radium, {StyleRoot} from 'radium';
+import { fadeIn } from 'react-animations';
+
+const styles = {
+	fadeIn: {
+	  animation: 'x 4s',
+	  animationName: Radium.keyframes(fadeIn, 'fadeIn')
+	}
+  }
 
 function App() {
   return (
+    <StyleRoot>
     <div className="App">
       <header className="App-header">
       <HeroText/>
        	  <div className = "profile-photo-container">
-	  <img src = {ProfilePhoto} className = "profile-photo"/>
-	  </div> 
+    <img src = {ProfilePhoto} style = {styles.fadeIn} className = "profile-photo"/>
+    </div> 
     <div className="title-text-container">
-    <TitleText text = "Links"/>
+        <TitleText text = "Links"/>
     </div>
     <div className="socialIcons">
     <SocialIcon className = "socialIcons" url = "https://linkedin.com/in/hamishsullivan"/>
     <SocialIcon className = "socialIcons" url = "https://github.com/hsull"/>
-  <p></p>
+    <p></p>
 
   </div>
+  <p></p>
+  <p></p>
+  <p></p>
+  <p></p>
+  <p></p>
+  <p></p>
+  <p></p>
+  <p></p>
+  <p></p>
+  <p></p>
+  <p></p>
+  <p></p>
+  <p></p>
+  <p></p>
+  <p></p>
+  <p></p>
+  <p></p>
+  <p></p>
+  <p></p>
+  <p></p>
+  <p></p>
+
       </header>
       <body>
         </body> 
 	  </div>
+    </StyleRoot>
+
   );
 }
 
--- src/components/HeroText/HeroText.js
+++ src/components/HeroText/HeroText.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import './HeroText.css';
+import { Parallax } from 'react-scroll-parallax';
+import useWindowDimensions from '../../useWindowDimensions';
+import { slideInLeft } from 'react-animations';
+import { slideInRight } from 'react-animations';
+
+import Radium, {StyleRoot} from 'radium';
+
+
+const styles = {
+	slideInLeft: {
+	  animation: 'x 2s',
+	  animationName: Radium.keyframes(slideInLeft, 'slideInLeft')
+	},
+	slideInRight: {
+		animation: 'x 2s',
+		animationName: Radium.keyframes(slideInRight, 'slideInRight')
+	  }
+  
+  }
+  
 function HeroText() {
 	return (
+	<StyleRoot>
 	<div class = "text">
-	<div class = "main-text-left">Hamish</div>
-	<div class = "main-text-right">Sullivan</div>
+	<div class = "main-text-left" style = {styles.slideInLeft}>Hamish</div>
+	<div class = "main-text-right" style = {styles.slideInRight}>Sullivan</div>
 	<div class = "sub-text"> Physics and Economics Student </div>
 	</div>
+	</StyleRoot>
 	);
 }
 
